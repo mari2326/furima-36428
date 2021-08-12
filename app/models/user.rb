56a354d0c8ecae5e169113f4f8284c6011664a11
mari2326 <pcoamo.o5@gmail.com>
2021-08-12 14:58:03 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validetes :nickname, presence: true
+  validetes :last_name, presence: true
+  validetes :first_name, presence: true
+  validetes :last_name_kana, presence: true
+  validetes :first_name_kana, presence: true
+  validetes :birthday, presence: true
+
 end
