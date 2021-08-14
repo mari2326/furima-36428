c97a0a6d6f232f08f069f01dbb9a2688eb79d1e6
mari2326 <pcoamo.o5@gmail.com>
2021-08-14 23:43:24 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,8 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category, :scheduled_state, :shipping_charge, :prefecture, :scheduled_day
+
+  validates :name, :explanation, :price, :image, presence: true
+
+  validates :category_id, :scheduled_state_id, :shipping_charge_id, :prefecture_id, :scheduled_day_id, numericality: { other_than: 1 , message: "can't be blank"}
 end
