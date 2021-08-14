5cf6cf3faed9377d59f008db8bb6c7e6c7b233af
mari2326 <pcoamo.o5@gmail.com>
2021-08-14 23:26:22 +0900

    ActiveHashの各ファイル作成と値とアソシエーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :scheduled_state, :shipping_charge, :prefecture, :scheduled_day
 end
