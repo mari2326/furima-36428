e3b20bd4efff24975c973d80c91d66d087437b63
mari2326 <pcoamo.o5@gmail.com>
2021-08-17 22:08:45 +0900

    編集画面のパスの設定/コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,10 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    @item = Item.find(params[:id])
+    if current_user.id != @item.user_id
+      redirect_to root_path
+    end
   end
 
   private
