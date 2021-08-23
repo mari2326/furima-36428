9ca6b0944d65faa95c9f60b1d9b767c3be66f9af
mari2326 <pcoamo.o5@gmail.com>
2021-08-23 11:08:07 +0900

    カラム名の追加/購入内容表示の編集/buysコントローラーの記述

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -2,5 +2,23 @@ class BuysController < ApplicationController
 
   def index
     @buy_address = BuyAddress.new
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @buy_address = BuyAddress.new(buy_params)
+    @item = Item.find(params[:item_id])
+    if @buy_address.valid?
+      @buy_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def buy_params
+    params.require(:buy_address).permit(:post_code, :prefecture_id, :municipality, :street_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])  
   end
 end
--- app/models/buy_address.rb
+++ app/models/buy_address.rb
@@ -4,7 +4,7 @@ class BuyAddress
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :municipality, :street_numbe, :user_id, :item_id
+    validates :municipality, :street_number, :user_id, :item_id
     validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid"}
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
@@ -13,3 +13,4 @@ class BuyAddress
     buy = Buy.create(user_id: user_id, item_id: item_id)
     Address.create(post_code: post_code, prefecture_id: prefecture_id, municipality: municipality, street_number: street_number, building_name: building_name, telephone_number: telephone_number, buy_id: buy.id)
   end
+end
