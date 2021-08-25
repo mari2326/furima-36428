380df4ab272783ceee020027dd4d362deda50de6
mari2326 <pcoamo.o5@gmail.com>
2021-08-25 12:52:43 +0900

    URL直接入力の際トップページに遷移する記述

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,13 +1,14 @@
 class BuysController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :create]
+  before_action :set_item, only: [:index, :create]
+  before_action :prevent_url, only: [:index, :create]
 
   def index
     @buy_address = BuyAddress.new
-    @item = Item.find(params[:item_id])
   end
 
   def create
     @buy_address = BuyAddress.new(buyaddress_params)
-    @item = Item.find(params[:item_id])
     if @buy_address.valid?
       pay_item
       @buy_address.save
@@ -23,6 +24,10 @@ class BuysController < ApplicationController
     params.require(:buy_address).permit(:post_code, :prefecture_id, :municipality, :street_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])  
   end
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"] 
     Payjp::Charge.create(
@@ -30,5 +35,11 @@ class BuysController < ApplicationController
       card: buyaddress_params[:token],  
       currency: 'jpy'       
     )
+  end
+    
+  def prevent_url
+    if @item.user_id == current_user.id || @item.buy != nil
+      redirect_to root_path
+    end
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+  before_action :prevent_url, only: [:edit, :update, :destroy]
 
 
   def index
@@ -54,7 +54,7 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def contributor_confirmation
+  def prevent_url
     if current_user.id != @item.user_id
       redirect_to root_path
     end
